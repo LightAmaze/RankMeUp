625b97c083ebc3095e812bd9a33d2fb7d41f64fd
lightamaze <lightamaze>
2016-10-23 15:39:10 -0500

    Added functionality to turn on and off build submission - default at op

--- src/main/java/com/lightamaze/rankmeup/RankMeUp.java
+++ src/main/java/com/lightamaze/rankmeup/RankMeUp.java
@@ -12,6 +12,7 @@ import com.scarsz.discordsrv.DiscordSRV;
 import com.scarsz.discordsrv.jda.entities.TextChannel;
 
 public final class RankMeUp extends JavaPlugin {
+	Boolean rmuDisabled = false;
     @Override
     public void onEnable() {
 
@@ -20,26 +21,46 @@ public final class RankMeUp extends JavaPlugin {
     @Override
     public void onDisable() {
     }
+    
+    
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	if (cmd.getName().equalsIgnoreCase("rankmeup")||(cmd.getName().equalsIgnoreCase("rmu"))) {
-    		//Player player = getServer().getPlayer(sender.getName());
-    		Player player = (Player)sender;
-    		TextChannel channel = DiscordSRV.getTextChannelFromChannelName("rankmeup");
-    		Location playerLocation = player.getLocation();
-    		double x = playerLocation.getX();
-    		double y = playerLocation.getY();
-    		double z = playerLocation.getZ();
-    		String world = playerLocation.getWorld().getName();
-    		String locationString = String.format("%s - %.2f, %.2f, %.2f", world, x, y, z);
-    		DiscordSRV.sendMessage(channel, "**"+sender.getName()+ "** is requesting to be ranked up for their build at coordinates **" + locationString+"** @here");
+    		if (!rmuDisabled) {
+    			//Player player = getServer().getPlayer(sender.getName());
+    			Player player = (Player)sender;
+    			TextChannel channel = DiscordSRV.getTextChannelFromChannelName("rankmeup");
+    			Location playerLocation = player.getLocation();
+    			double x = playerLocation.getX();
+    			double y = playerLocation.getY();
+    			double z = playerLocation.getZ();
+    			String world = playerLocation.getWorld().getName();
+    			String locationString = String.format("%s - %.2f, %.2f, %.2f", world, x, y, z);
+    			DiscordSRV.sendMessage(channel, "**"+sender.getName()+ "** is requesting to be ranked up for their build at coordinates **" + locationString+"** @here");
+    			sender.sendMessage("You have requested for an admin to grade your build at "+ChatColor.YELLOW+locationString+ChatColor.WHITE+". Please be patient, it may take a while.");
+    		}
+    		else {
+    			sender.sendMessage(ChatColor.RED+"RankMeUp submission is currently disabled.");
+    		}
+    	}
+	
+    	if (cmd.getName().equalsIgnoreCase("rmutoggle")) {
+    		if (!rmuDisabled) {
+    			sender.sendMessage(ChatColor.YELLOW+"RankMeUp submission is now disabled.");
+    			rmuDisabled = true;
+    		}
+    		else {
+    			sender.sendMessage(ChatColor.YELLOW+"RankMeUp submission is now enabled.");
+    			rmuDisabled = false;
+    		}
+    	}
 
-    		sender.sendMessage("You have requested for an admin to grade your build at "+ChatColor.YELLOW+locationString+ChatColor.WHITE+". Please be patient, it may take a while.");
-    		return true;
-    	} 
-
-    	return false; 
+    	
+			
+    	return true;
     }
-
 }
+    	
+
+
 	
