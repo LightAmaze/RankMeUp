66e5ede1e743e973e1e29dca6626c80d4b18b9f7
lightamaze <lightamaze>
2016-10-22 23:11:44 -0500

    initial commit

---
+++ src/main/java/com/lightamaze/rankmeup/RankMeUp.java
@@ -0,0 +1,45 @@
+package com.lightamaze.rankmeup;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.scarsz.discordsrv.DiscordSRV;
+import com.scarsz.discordsrv.jda.entities.TextChannel;
+
+public final class RankMeUp extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    	
+    }
+    @Override
+    public void onDisable() {
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("rankmeup")) {
+    		//Player player = getServer().getPlayer(sender.getName());
+    		Player player = (Player)sender;
+    		TextChannel channel = DiscordSRV.getTextChannelFromChannelName("rankmeup");
+    		Location playerLocation = player.getLocation();
+    		double x = playerLocation.getX();
+    		double y = playerLocation.getY();
+    		double z = playerLocation.getZ();
+    		String world = playerLocation.getWorld().getName();
+    		String locationString = String.format("%s - %.2f, %.2f, %.2f", world, x, y, z);
+    		DiscordSRV.sendMessage(channel, "**"+sender.getName()+ "** is requesting to be ranked up for their build at coordinates **" + locationString+"** @here");
+
+    		sender.sendMessage("You have requested for an admin to grade your build at "+ChatColor.YELLOW+locationString+ChatColor.WHITE+". Please be patient, it may take a while.");
+    		return true;
+    	} 
+
+    	return false; 
+    }
+
+}
+	
